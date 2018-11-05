3ff37d42af38d8a69a900ff6d3baf676a3890b73
Martin Horciak <martinhorciak@martins-macbook-pro.local>
2018-11-05 09:37:24 -0500

    Initial Commit

---
+++ Horciak_Martin_Checklist/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Horciak_Martin_Checklist
+//
+//  Created by Martin Horciak on 2018-11-05.
+//  Copyright © 2018 Martin Horciak. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
